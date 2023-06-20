05370cd6c6813f774ea778e254f265b4a47bea5d
onifernando1 <onifernando1@gmail.com>
2023-06-20 20:33:20 +0100

    paintings controller

--- server/controllers/paintingsController.js
+++ server/controllers/paintingsController.js
@@ -9,30 +9,38 @@ exports.painting_list = asyncHandler(async (req, res, next) => {
 
 exports.ink = asyncHandler(async (req, res, next) => {
   const inkPaintings = await Painting.find({ category: "ink" }).exec();
-  res.json({ inkPaintings: inkPaintings });
+  res.json({ painting_list: inkPaintings });
 });
 
 exports.abstract = asyncHandler(async (req, res, next) => {
-  const inkPaintings = await Painting.find({ category: "ink" }).exec();
-  res.json({ inkPaintings: inkPaintings });
+  const abstractPaintings = await Painting.find({
+    category: "abstract",
+  }).exec();
+  res.json({ painting_list: abstractPaintings });
 });
 
 exports.landscape = asyncHandler(async (req, res, next) => {
-  const allPaintings = await Painting.find({}).exec();
-  res.json({ painting_list: allPaintings });
+  const landscapePaintings = await Painting.find({
+    category: "landscape",
+  }).exec();
+  res.json({ painting_list: landscapePaintings });
 });
 
 exports.figurative = asyncHandler(async (req, res, next) => {
-  const allPaintings = await Painting.find({}).exec();
-  res.json({ painting_list: allPaintings });
+  const figurativePaintings = await Painting.find({
+    category: "figurative",
+  }).exec();
+  res.json({ painting_list: figurativePaintings });
 });
 
 exports.portrait = asyncHandler(async (req, res, next) => {
-  const allPaintings = await Painting.find({}).exec();
-  res.json({ painting_list: allPaintings });
+  const portraitPaintings = await Painting.find({
+    category: "portrait",
+  }).exec();
+  res.json({ painting_list: portraitPaintings });
 });
 
 exports.other = asyncHandler(async (req, res, next) => {
-  const allPaintings = await Painting.find({}).exec();
-  res.json({ painting_list: allPaintings });
+  const otherPaintings = await Painting.find({ category: "other" }).exec();
+  res.json({ painting_list: otherPaintings });
 });

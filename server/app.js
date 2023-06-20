f85cb341876b3ddca80b69589c23253bb3c1713a
onifernando1 <onifernando1@gmail.com>
2023-06-20 20:00:58 +0100

    add paiting router

--- server/app.js
+++ server/app.js
@@ -5,9 +5,9 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const mongoose = require("mongoose");
 require("dotenv").config();
-
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+const paintingsRouter = require("./routes/paintings");
 
 var app = express();
 
@@ -31,6 +31,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/paintings", paintingsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- server/routes/paintings.js
+++ server/routes/paintings.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", function (req, res) {
-  res.send("Wiki home page");
+router.get("/", function (req, res, next) {
+  res.send("PAINTING");
 });
 
-router.get("/about", function (req, res) {
-  res.send("About this wiki");
-});
+module.exports = router;

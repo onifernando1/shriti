b8308f32a346b158700c5737ce64edeb88a73b27
onifernando1 <onifernando1@gmail.com>
2023-06-20 21:21:55 +0100

    layout portfolio in divs

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -18,9 +18,19 @@ function Portfolio() {
 
   return (
     <>
-      {paintings.map((painting) => {
-        return <div>{painting.title}</div>;
-      })}
+      <div className="portfolio-container">
+        <div className="portfolio-paintings-container">
+          {paintings.map((painting) => {
+            return (
+              <div className="portfolio-individual-container">
+                <img
+                  src={require(`../assets/images/${painting.image}.jpg`)}
+                ></img>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }

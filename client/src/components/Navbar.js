3bba2802cfe4a6e6fae7de4ea716c31f74ed1af9
onifernando1 <onifernando1@gmail.com>
2023-06-20 22:02:12 +0100

    very basic navbar styling, flexbox, text dec none

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
+import "../assets/styles/navbar.css";
 
 function Navbar() {
   return (
     <>
-      <Link to="/">Home</Link>
-      <Link to="/portfolio">Portfolio</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
-      <Link to="/news">News</Link>
-      <Link to="/workshops">Workshops</Link>
+      <div className="navbar-container">
+        <Link to="/">Home</Link>
+        <Link to="/portfolio">Portfolio</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/news">News</Link>
+        <Link to="/workshops">Workshops</Link>
+      </div>
     </>
   );
 }
--- client/src/components/PaintingOverview.js
+++ client/src/components/PaintingOverview.js
@@ -2,7 +2,6 @@ function PaintingOverview(params) {
   const paintings = params.paintings;
   return (
     <div className="portfolio-container">
-      <div className="header-temp">HEADER HEADER HEADER </div>
       <div className="portfolio-paintings-container">
         {paintings.map((painting) => {
           return (

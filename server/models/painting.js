a36db204312266ab0e45e7bf608c19e36ad1b75f
onifernando1 <onifernando1@gmail.com>
2023-06-20 19:51:27 +0100

    create painting schema

---
+++ server/models/painting.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const PaintingSchema = new Schema({
+  image: { type: String },
+  title: { type: String },
+  description: { type: String },
+  category: { type: String },
+  information: { type: String },
+});
+
+module.exports = mongoose.model("Painting", PaintingSchema);

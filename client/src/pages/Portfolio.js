5c83c4afea31d617c0a0a4a4ae06eab7074da118
onifernando1 <onifernando1@gmail.com>
2023-06-20 21:15:39 +0100

    cors, json in backedn

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,5 +1,22 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState, setState } from "react";
+import axios from "axios";
+
 function Portfolio() {
-  return <div>Homepage</div>;
+  const [paintings, setPaintings] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/paintings`)
+      .then((response) => {
+        setPaintings(response.data.painting_list);
+        console.log(`painting list: ${paintings}`);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+  return <div>Portfolio page</div>;
 }
 
 export default Portfolio;

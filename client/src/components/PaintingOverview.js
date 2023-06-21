6a9f591abff8219ff7155926f1c97992504a6da6
onifernando1 <onifernando1@gmail.com>
2023-06-21 15:43:07 +0100

    logic to place full image into div on click

--- client/src/components/PaintingOverview.js
+++ client/src/components/PaintingOverview.js
@@ -1,18 +1,37 @@
+import "../assets/styles/overview.css";
+import { useState, setState } from "react";
+
 function PaintingOverview(params) {
   const paintings = params.paintings;
+  const [fullScreenImage, setFullScreenImage] = useState("");
+
+  const displayFullScreenImage = (name) => {
+    setFullScreenImage(name);
+  };
+
   return (
     <div className="portfolio-container">
       <div className="portfolio-paintings-container">
         {paintings.map((painting) => {
           return (
-            <div className="portfolio-individual-container">
+            <div
+              className="portfolio-individual-container"
+              key={painting._id}
+              id={painting.image}
+            >
               <img
+                onClick={() => displayFullScreenImage(painting.image)}
                 src={require(`../assets/images/${painting.image}.jpg`)}
               ></img>
             </div>
           );
         })}
       </div>
+      {fullScreenImage ? (
+        <div className="full-screen-image">
+          <img src={require(`../assets/images/${fullScreenImage}.jpg`)}></img>
+        </div>
+      ) : null}
     </div>
   );
 }

fe1448cabd321acef9584b22dc2ead6387584812
onifernando1 <onifernando1@gmail.com>
2023-06-21 22:14:46 +0100

    news

--- client/src/pages/News.js
+++ client/src/pages/News.js
@@ -1,5 +1,11 @@
+require("../assets/styles/news.css");
 function News() {
-  return <div>News</div>;
+  return (
+    <div className="news-container">
+      <div className="upcoming">Upcoming events</div>
+      <img className="news" src={require("../assets/images/kaos.jpg")}></img>
+    </div>
+  );
 }
 
 export default News;

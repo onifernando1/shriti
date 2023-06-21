2206e4ec4e6c575059a5ab406f8a4ef9e9033c73
onifernando1 <onifernando1@gmail.com>
2023-06-21 22:20:44 +0100

    workshops info

--- client/src/pages/News.js
+++ client/src/pages/News.js
@@ -2,7 +2,7 @@ require("../assets/styles/news.css");
 function News() {
   return (
     <div className="news-container">
-      <div className="upcoming">Upcoming events</div>
+      <div className="upcoming">upcoming events</div>
       <img className="news" src={require("../assets/images/kaos.jpg")}></img>
     </div>
   );
--- client/src/pages/Workshops.js
+++ client/src/pages/Workshops.js
@@ -1,5 +1,20 @@
 function Workshops() {
-  return <div>Workshops</div>;
+  return (
+    <div>
+      <div className="workshop-container">
+        <div>
+          <div>upcoming workshops</div>
+          <div>sunday 25 june - textured art</div>
+        </div>
+        <div>
+          <div>past workshops</div>
+          <div>3rd june - textured art</div>
+          <img></img>
+          <div>PHOTOS</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Workshops;

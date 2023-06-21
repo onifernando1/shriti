2a13ef19fd0a883dc5c14d4c5163791953c39975
onifernando1 <onifernando1@gmail.com>
2023-06-21 22:26:46 +0100

    workshops

--- client/src/pages/Workshops.js
+++ client/src/pages/Workshops.js
@@ -1,13 +1,15 @@
+require("../assets/styles/workshop.css");
+
 function Workshops() {
   return (
     <div>
       <div className="workshop-container">
-        <div>
-          <div>upcoming workshops</div>
+        <div className="upcoming">
+          <div id="workshop-title">upcoming workshops</div>
           <div>sunday 25 june - textured art</div>
         </div>
-        <div>
-          <div>past workshops</div>
+        <div className="past">
+          <div id="workshop-title">past workshops</div>
           <div>3rd june - textured art</div>
           <img></img>
           <div>PHOTOS</div>

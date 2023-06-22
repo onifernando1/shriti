eda8afa3bd8364b148e69e82d9d0f0fcd58de3a5
onifernando1 <onifernando1@gmail.com>
2023-06-22 16:01:31 +0100

    frontend pages new categories

---
+++ client/src/pages/Seascape.js
@@ -0,0 +1,29 @@
+import { isMobile } from "react-device-detect";
+import React from "react";
+import { useEffect } from "react";
+import { useState, setState } from "react";
+import axios from "axios";
+import "../assets/styles/overview.css";
+import PaintingOverview from "../components/PaintingOverview";
+
+function Seascape() {
+  const [paintings, setPaintings] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/paintings/seascape`)
+      .then((response) => {
+        setPaintings(response.data.painting_list);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <>
+      <PaintingOverview paintings={paintings} />
+    </>
+  );
+}
+
+export default Seascape;

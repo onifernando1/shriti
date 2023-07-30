7ec1dd902865a685b183350c2967905a0ad23b52
onifernando1 <onifernando1@gmail.com>
2023-07-30 20:13:40 +0100

    add new news and style

--- client/src/pages/News.js
+++ client/src/pages/News.js
@@ -3,6 +3,10 @@ function News() {
   return (
     <div className="news-container">
       <div className="upcoming-news">upcoming events</div>
+      <div className="written-news">
+        <div>KAOS @ Rose Theatre</div>
+        <div>4th October - 4th November</div>
+      </div>
       <img className="news" src={require("../assets/images/kaos.jpg")}></img>
     </div>
   );
